5a2955b6948b8670e57ecc37f6647e977eceb17f
Simon <simonmonaco86@gmail.com>
2025-04-08 18:39:52 -0300

    practica 3 ejercicio 6

---
+++ Practica3/ejercicio6.pas
@@ -0,0 +1,23 @@
+Program ejercicio6;
+procedure Par;
+var
+    maximo:integer; numero:integer;
+begin
+    maximo:=-1;
+    writeln('escriba un numero: ');
+    readln(numero);
+    while(numero>=0)do
+    begin
+        if(numero MOD 2 = 0)then begin
+            if(numero > maximo)then
+                maximo:=numero;
+        end;
+        writeln('escriba un numero: ');
+        readln(numero);
+    end;
+    writeln(maximo);
+end;
+
+begin
+    Par;
+end.
